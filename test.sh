719db6809fcd4b69b87870855300a216cc303b97
Martin Pool <mbp@sourcefrog.net>
2013-01-19 20:44:12 +1100

    Run all tests before aborting, even if some fail

--- test.sh
+++ test.sh
@@ -1,18 +1,20 @@
 #! /bin/zsh -e
 
+failed=false
 for infile in test/parse/*.in
 do
     echo $infile ...
     outfile=${infile:r}.out
-    ./loss_parse $infile | diff -u $outfile -
+    ./loss_parse $infile | diff -u $outfile - || failed=true
 done
-
+$failed && exit 1
 
 for infile in test/eval/*.scm
 do
     echo $infile ...
     outfile=${infile:r}.out
-    ./loss $infile | diff -u $outfile -
+    ./loss $infile | diff -u $outfile - || failed=true
 done
+$failed && exit 1
 
 echo victory!
